6847c29f1e3e7cbb35f44fbb539f67f9b485a577
ip <ipdev99@gmail.com>
2022-01-17 22:53:42 -0600

    WIP

--- list_mModules.sh
+++ list_mModules.sh
@@ -22,65 +22,110 @@ SCRIPT=list_mModules.sh
 
 # Set additional variables
 OUT=mModules_$DATE.txt
-# MINMAGISKVER=23010
+# MINVCODE=23010
 
 # Set main functions
 
 check_android_device() {
     [ -f /system/bin/sh ] || [ -f /system/bin/toybox ] || [ -f /system/bin/toolbox ] && ANDROID=TRUE;
-    if [[ -z $ANDROID ]]; then
+    if [ -z "$ANDROID" ]; then
         echo ""; echo " This script needs to be run on an Android device. "; echo "";
-        exit 0;
-    fi;
+        exit 0
+    fi
 }
 
 set_target_directory() {
     if [ ! -f "$SCRIPT" ]; then
-        TDIR=$(lsof 2>/dev/null | grep -o '[^ ]*$' | grep -m1 "$SCRIPT" | sed 's/\/'"$SCRIPT"'//g');
-        cd $TDIR;
-    fi;
+        TDIR=$(lsof 2>/dev/null | grep -o '[^ ]*$' | grep -m1 "$SCRIPT" | sed 's/\/'"$SCRIPT"'//g')
+        cd $TDIR
+    fi
 }
 
 # Set additional functions
 
-# check_magisk_ver(){
-#     MAGISKVER=$(magisk -V 2> /dev/null);
-#     if [[ -z $MAGISKVER ]]; then
-#         echo ""; echo " Requires Magisk to be installed and active. "; echo "";
-#         exit 0;
-#     else
-#         MAGISKBLD=$(magisk -v | cut -f1 -d ':');
-#     fi;
+check_magisk_ver() {
+    MAGISKVCODE=$(magisk -V 2> /dev/null)
+    if [ -z "$MAGISKVCODE" ]; then
+        echo ""; echo " Requires Magisk to be installed and active. "; echo "";
+        exit 0
+    else
+        MAGISKBLD=$(magisk -v | cut -f1 -d ':')
+    fi
 
-#     # if [[ $MAGISKVER -lt $MINMAGISKVER ]]; then
-#     #   echo ""; echo " Requires Magisk "$MINMAGISKVER" to be installed and active. ";
-#     #   echo "  Installed version is "$MAGISKBLD; echo "";
-#     #   exit 0;
-#     # fi;
-# }
+    # if [ "$MAGISKVCODE" -lt "$MINVCODE" ]; then
+    #   echo ""; echo " Requires Magisk ("$MINVCODE") to be installed and active. ";
+    #   echo "  Installed version is ("$MAGISKVCODE")"; echo "";
+    #   exit 0;
+    # fi;
+}
+
+check_modules() {
+    if [ ! "$(find /data/adb/modules/ -mindepth 1 -name 'module.prop')" ]; then
+        echo ""; echo " No Magisk modules installed."; echo ""
+        exit 0
+    else
+        echo "List of installed Magisk modules." > $OUT
+        echo " "$(date '+%Y %B %d') >> $OUT
+        echo " Current Magisk is "$(magisk -c) >> $OUT
+        echo "" >> $OUT
+    fi
+}
+
+get_module_prop() {
+    mID=$(grep id= $i | cut -f2 -d "=")
+    mName=$(grep name= $i | cut -f2 -d "=")
+    mVer=$(grep version= $i | cut -f2 -d "=" | sed 's/^v//')
+    mVerCode=$(grep versionCode= $i | cut -f2 -d "=")
+    mAuthor=$(grep author= $i | cut -f2 -d "=")
+    # echo $mName" v."$mVer >> $OUT
+    echo $mName" "$mVer >> $OUT
+    echo "["$mID" ("$mVerCode")]" >> $OUT
+    echo "by "$mAuthor >> $OUT
+    echo "" >> $OUT
+}
 
 # Lets go.
 
 # Determine if running on an Android device.
-check_android_device;
+check_android_device
 
 # Reset and move to the target directory if needed.
-set_target_directory;
+set_target_directory
 
-# # Determine if Magisk is installed and running.
-# check_magisk_ver;
+# Determine if Magisk is installed and running.
+check_magisk_ver
 
-# Cat each module prop file.
+# Check if Magisk modules are installed. Exit or start the list file.
+check_modules
+
+# # Cat each module prop file.
+# for i in $(find /data/adb/modules/ -mindepth 1 -name 'module.prop'); do
+#     mPath=$(printf $i | sed 's/module.prop//g');
+#     # [ -f "$mPath""disable" ] && echo " ## This module is disabled. ";
+#     if [ -f "$mPath""disable" ]; then
+#         ii=$(cat $i | grep id= | cut -f2 -d'=');
+#         echo " ## "$ii" module is disabled." >>$OUT;
+#     fi;
+#     cat $i >> $OUT;
+#     echo "" >> $OUT;
+# done;
+
+# Add enabled Magisk modules to the list.
 for i in $(find /data/adb/modules/ -mindepth 1 -name 'module.prop'); do
-    mPath=$(printf $i | sed 's/module.prop//g')
-    # [ -f "$mPath""disable" ] && echo " ## This module is disabled. ";
-    if [ -f "$mPath""disable" ]; then
-        ii=$(cat $i | grep id= | cut -f2 -d'=');
-        echo " ## "$ii" module is disabled." >>$OUT;
-    fi;
-    cat $i >> $OUT;
-    echo "" >> $OUT;
-done;
+    ii=$(printf $i | sed 's/module.prop//g')
+    [ ! -f "$ii""disable" ] && get_module_prop
+done
+
+# Add disabled Magisk modules to the list.
+if [ ! -z "$(find /data/adb/modules/ -mindepth 1 -name 'disable')" ]; then
+    echo "" >> $OUT
+    echo " Disabled modules." >> $OUT
+    echo "" >> $OUT
+    for i in $(find /data/adb/modules/ -mindepth 1 -name 'module.prop'); do
+        ii=$(printf $i | sed 's/module.prop//g')
+        [ -f "$ii""disable" ] && get_module_prop
+    done
+fi
 
 # Finish script
 echo ""; echo " Done."; echo "";
