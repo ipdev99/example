f208d100cd8fc786b126487a7bb129da2988fea7
ip <ipdev99@gmail.com>
2022-02-05 16:26:54 -0600

    Example of output file.

---
+++ SystemlessDebloaterList.sh
@@ -0,0 +1,11 @@
+# Source list for Systemless Debloater (REPLACE)
+# Copyright (c) zgfg @ xda, 2020-2022
+
+VerboseLog="true"
+
+DebloatList="
+Calculator
+CalculatorGooglePrebuilt
+YouTube
+YouTubeMusicPrebuilt
+"
