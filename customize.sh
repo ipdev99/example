d1cf70e862759945dff863fc0e345a2e9eb46318
ipdev99 <ipdev99@gmail.com>
2023-04-16 21:11:14 -0500

    customize script

--- customize.sh
+++ customize.sh
@@ -1,17 +1,33 @@
-# Set owner, group, permission and security.
-set_perm "$MODPATH"/system/addon.d/99-magisk.sh 0 0 0755 u:object_r:system_file:s0
-# set_perm "$MODPATH"/system/addon.d/99-magisk.sh 0 0 0644 u:object_r:system_file:s0
-# set_perm "$MODPATH"/system/addon.d/99-magisk.sh 1000 1000 0644 u:object_r:system_file:s0
+#!/system/bin/sh
 
-# -rw-r--r--  1 root root u:object_r:system_file:s0    710 2008-12-31 18:00 50-lineage.sh
-# -rwxr-xr-x  1 root root u:object_r:system_file:s0  11281 1973-12-22 07:50 70-gapps.sh
+## Magisk OTA Survival module.
+## This module will overlay Magisk's addon.d script into the system/addon.d directory if it exists.
+## ipdev @ xda-developers
 
-# -rw-r--r--  1 root root u:object_r:system_file:s0    710 2008-12-31 18:00 50-lineage.sh
-# -rwxr-xr-x  1 root root u:object_r:system_file:s0  11281 1973-12-22 07:50 70-gapps.sh
-# -rw-r--r--  1 root root u:object_r:system_file:s0   3534 2023-04-15 21:21 99-magisk.sh
+# __ Define variables. __
 
-# -rwxr-xr-x  1 root root u:object_r:system_file:s0    728 2023-04-15 22:00 50-lineage.sh
-# -rwxr-xr-x  1 root root u:object_r:system_file:s0  11299 2023-04-15 22:00 70-gapps.sh
-# -rwxr-xr-x  1 root root u:object_r:system_file:s0   3534 2023-04-15 21:46 99-magisk.sh
+### Magisk variables
+## MODPATH (path): the path where your module files should be installed.
 
-## Why did 50-linage change permissions?
+# ADB Directory
+ADB=/data/adb
+
+# __ Define functions. __
+
+### Magisk functions
+## set_perm <target> <owner> <group> <permission> [context]
+
+# __ Here we go. __
+
+if [ -d /system/addon.d ]; then
+	# Copy the current addon.d script.
+	cp "$ADB"/magisk/addon.d.sh "$MODPATH"/system/addon.d/99-magisk.sh
+
+	# Set owner, group, permission and security.
+	set_perm "$MODPATH"/system/addon.d/99-magisk.sh 0 0 0755 u:object_r:system_file:s0
+else
+	echo "This device does not support addon.d scripts."
+	exit 1
+fi
+
+# __ Finish and Cleanup. __
